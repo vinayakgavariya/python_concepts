8b33cdacf707277413570db8245fa7656410c56e
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-04-06 08:00:01 +0530

    loop file

---
+++ loop.py
@@ -0,0 +1,16 @@
+#loop
+i=5 #intialization
+
+while i>=1: #condition
+    print("she", end=" ")
+    j=1
+    while j<=4: #nested while loop
+        print("rocks", end=" ")
+        j=j+1
+        break
+    i=i-1
+    print()
+  #increment or decrement
+
+  # end=" " is used when we don't want value in next line 
+  
