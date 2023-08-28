7caa9f14019a9100efc47aadadfc9f343c940f1c
Vinayak Gavariya <77708101+vinayakgavariya@users.noreply.github.com>
2023-08-28 10:13:03 +0530

    Create hash_function.py

---
+++ hash_function.py
@@ -0,0 +1,8 @@
+"""Python hash() function is a built-in function and returns the hash value of an object if it has one. The hash value is an integer which is used to quickly compare dictionary keys while looking at a dictionary."""
+#Syntax : hash(obj)
+
+#Parameters :  obj : The object which we need to convert into hash.
+
+#Returns : Returns the hashed value if possible. 
+
+# more here:- https://www.geeksforgeeks.org/python-hash-method/
