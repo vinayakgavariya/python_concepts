724348a58f359828721b12296b324b971199a2c2
Vinayak Gavariya <77708101+vinayakgavariya@users.noreply.github.com>
2023-08-30 10:21:28 -0700

    Update set_add.py

--- HackerRank_Problems/sets/set_add.py
+++ HackerRank_Problems/sets/set_add.py
@@ -1,7 +1,9 @@
-s= input(int())
-for x in  s:
-    set=input()
-print(set)
+n = int(input())  # Read the total number of country stamps
+country_stamps = set()  # Create an empty set to store distinct country names
 
-    
+for _ in range(n):
+    country_name = input()  # Read the country name for each stamp
+    country_stamps.add(country_name)  # Add the country name to the set
 
+total_distinct_stamps = len(country_stamps)  # Get the count of distinct stamps
+print(total_distinct_stamps)  # Print the total number of distinct country stamps
