c4195ec02fcc9de20b8ad100f3b63fc1699c006d
vinayakgavariya <vinayakgavariya123@gmai.com>
2023-09-11 08:10:14 +0530

    learnt some more python today

---
+++ HackerRank_Problems/itertools/itertools_product.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import product
+
+A= list(map(int, input().split()))
+B= list(map(int, input().split()))
+
+result= (product(A,B))
+
+for item in result:
+    print(item, end=' ')
